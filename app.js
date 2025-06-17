6a51a5dddae2120a4c521ca8ed2d6e919ead6229
omnya zerghouni <omnyazer@gmail.com>
2025-06-17 10:45:32 +0200

    Ajout de la route DELETE /users/:id

--- app.js
+++ app.js
@@ -4,26 +4,8 @@ const port = 3000;
 
 app.use(express.json());
 
-// Route racine
 app.get('/', (req, res) => {
   res.send('Hello World');
-});
-
-// Route GET /users
-app.get('/users', (req, res) => {
-  res.json([
-    { id: 1, name: 'Alice' },
-    { id: 2, name: 'Bob' }
-  ]);
-});
-
-// Route POST /users
-app.post('/users', (req, res) => {
-  const newUser = req.body;
-  res.status(201).json({
-    message: 'Nouvel utilisateur ajouté',
-    user: newUser
-  });
 });
 
 // Route DELETE /users/:id
@@ -32,12 +14,6 @@ app.delete('/users/:id', (req, res) => {
   res.json({ message: `Utilisateur ${userId} supprimé` });
 });
 
-// Gestion des routes inexistantes
-app.use((req, res) => {
-  res.status(404).json({ error: 'Route non trouvée' });
-});
-
-// Démarrage du serveur
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
